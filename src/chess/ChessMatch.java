ca59cb5514caa7dbb17dd5e8a759dbc897631480
ahvShiro <arthurshiro123@gmail.com>
2025-05-29 14:48:01 -0300

    Chess pieces add ed to initialSetup(), implementation pending

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,16 +25,53 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        // KINGS
+        // ==== WHITE ====================================
+        // KING
         board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+
+        // QUEEN
+//        board.placePiece(new Queen(board, Color.BLACK), new Position(7, 3));
 
         // THE ROOOOOOOOOOOOOOOOOOOOK
         board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
         board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
 
+        // KNIGHT
+//        board.placePiece(new Knight(board, Color.WHITE), new Position(7, 1));
+//        board.placePiece(new Knight(board, Color.WHITE), new Position(7, 6));
+
+        // BISHOP
+//        board.placePiece(new Bishop(board, Color.WHITE), new Position(7, 2));
+//        board.placePiece(new Knight(board, Color.WHITE), new Position(7, 5));
+
+        // PAWNS
+//        for (int i = 0; i < board.getRows(); i++) {
+//            board.placePiece(new Pawn(board, Color.WHITE), new Position(6, i));
+//        }
+
+        // ==== BLACK ====================================
+        // KING
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+
+        // QUEEN
+//        board.placePiece(new Queen(board, Color.BLACK), new Position(0, 3));
+
+        // THE ROOOOOOOOOOOOOOOOOOOOK
         board.placePiece(new Rook(board, Color.BLACK), new Position(0, 7));
         board.placePiece(new Rook(board, Color.BLACK), new Position(0, 0));
+
+        // KNIGHT
+//        board.placePiece(new Knight(board, Color.WHITE), new Position(0, 1));
+//        board.placePiece(new Knight(board, Color.WHITE), new Position(0, 6));
+
+        // BISHOP
+//        board.placePiece(new Bishop(board, Color.WHITE), new Position(0, 2));
+//        board.placePiece(new Knight(board, Color.WHITE), new Position(0, 5));
+
+        // PAWNS
+//        for (int i = 0; i < board.getRows(); i++) {
+//            board.placePiece(new Pawn(board, Color.WHITE), new Position(1, i));
+//        }
     }
 
 }
